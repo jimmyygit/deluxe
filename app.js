078d30ce5c31b49765d7016d80a27c271ef6fcb3
Jimmy Yang <jimmyy1337@gmail.com>
2022-02-05 15:43:05 -0500

    ejs partials

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res
+    .status(404)
+    .render("404", { pageTitle: "Page Not Found", path: "not-found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,7 @@ router.get("/", (req, res, next) => {
   res.render("shop", {
     pageTitle: "Product",
     products: adminData.products,
+    path: "/",
   });
 });
 
